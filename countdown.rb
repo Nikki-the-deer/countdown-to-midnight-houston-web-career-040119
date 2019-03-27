4a86f0bb889ef786c044d1404c50dea39f389b46
Nicholas Eubanks <nick-2013@live.com>
2019-03-27 17:15:06 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
 
-def countdown
+def countdown(int)
+  while int > 0 do
+    puts "#{int} SECOND(S)!"
+    if int == 0 do 
+      break
+    end
+  end
+  puts "HAPPY NEW YEAR!"
 end
